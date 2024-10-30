934db38e086a81ba4aa2744f39fa72e560cdb1a7
Dmitry Slobodchikov <dmitryslo@yandex.ru>
2024-10-30 23:47:08 +0500

    Complete lab03

--- MLOps_LR02/lr3/shell/extract.sh
+++ MLOps_LR02/lr3/shell/extract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python MLOps_LR02/lr3/src/extract.py extract_data.csv MLOps_LR02/lr3/data/extracted.csv
+poetry run python MLOps_LR02/lr3/src/extract.py extract_data.csv MLOps_LR02/lr3/data/extracted.csv
--- MLOps_LR02/lr3/shell/load.sh
+++ MLOps_LR02/lr3/shell/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python MLOps_LR02/lr3/src/load.py MLOps_LR02/lr3/data/transformed.csv loaded.csv
+poetry run python MLOps_LR02/lr3/src/load.py MLOps_LR02/lr3/data/transformed.csv loaded.csv
